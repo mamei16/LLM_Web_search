99e6bff27a2f9c5401f21731f2a8930413f19339
marcel <marcel.1710@live.de>
2024-04-17 23:36:34 +0200

    fix positioning of "force web search" checkbox and change loading method of custom javascript

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let elem = document.getElementById("Generate");
-elem.insertAdjacentHTML("afterend", '<label style="color:#8b8b8b;white-space:nowrap;position:absolute;top:85px;bottom:0;left:-45px;margin:auto;"><input type="checkbox" id="force-search" name="accept">  Force web search </label>');
+var generate_button = document.getElementById("Generate");
+generate_button.insertAdjacentHTML("afterend", '<div style="position:relative;"> <label style="color:#8b8b8b;white-space:nowrap;position:absolute;top:8px;right:3px;"><input type="checkbox" id="force-search" name="accept">  Force web search </label> </div>');
+generate_button.style.setProperty("position", "relative");
+generate_button.style.setProperty("top", "15px");
+generate_button.style.setProperty("margin-left", "-10px");
+
+var stop_button = document.getElementById("stop");
+stop_button.style.setProperty("position", "relative");
+stop_button.style.setProperty("top", "15px");
+stop_button.style.setProperty("margin-left", "-10px");
+
 var checkbox = document.getElementById("force-search");
 var gradio_force_search_checkbox = document.getElementById("Force-search-checkbox").children[1].firstChild;
 checkbox.addEventListener('change', function() {
