968a5dd9fc30227b2ddfb6d96c3ce543b07d4079
marme <marcel.1710@live.de>
2024-10-30 00:21:15 +0100

    revert accidental edit in request header

--- retrieval.py
+++ retrieval.py
@@ -165,7 +165,7 @@ async def async_download_html(url, headers):
 
 
 async def async_fetch_urls(urls):
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:122340.0) Gecko/20100101 Firefox/120.0",
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0",
                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
                "Accept-Language": "en-US,en;q=0.5"}
     webpages = await asyncio.gather(*[(async_download_html(url, headers)) for url in urls])
